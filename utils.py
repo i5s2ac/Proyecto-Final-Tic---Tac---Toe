153a9759b4f53f8aa3459893ef921a13558660e3
i5s2ac <103969157+i5s2ac@users.noreply.github.com>
2022-05-13 11:46:52 -0600

    Ataque : Horizontales
    Ataque para hacer horizontales.

--- utils.py
+++ utils.py
@@ -104,7 +104,48 @@ def decide_move(board: list, player_id: str) -> [int, int]:
         
         return [1, 1]
 
+    # ---------------------------------------------- | ATACAR: HORIZONTALES | ---------------------------------------------- 
+    
+    # Seguidamente, buscaremos las horizontales: Esto se hace con la finalidad de analizar los diferentes escenarios que existen para completar una linea de tres y tras anaizarlo, proceder a completar dicha linea.
+    
+    #Si las posiciones [0,0] y [0,1] ya estan llenados por nuestro player ID y la posición [0,2] esta vacia, entonces, retornar la posición [0,2].
+    elif board[0][0] == player_id and board[0][1] == player_id and board[0][2] == '-':
+        return [0, 2]
+    
+    #Si las posiciones [0,0] y [0,2] ya estan llenados por nuestro player ID y la posición [0,1] esta vacia, entonces, retornar la posición [0,1].
+    elif board[0][0] == player_id and board[0][2] == player_id and board[0][1] == '-':
+        return [0, 1]
+    
+    #Si las posiciones [0,2] y [0,1] ya estan llenados por nuestro player ID y la posición [0,0] esta vacia, entonces, retornar la posición [0,0].
+    elif board[0][2] == player_id and board[0][1] == player_id and board[0][0] == '-':
+        return [0, 0]
 
+    #Si las posiciones [1,0] y [1,1] ya estan llenados por nuestro player ID y la posición [1,2] esta vacia, entonces, retornar la posición [1,2].
+    elif board[1][0] == player_id and board[1][1] == player_id and board[1][2] == '-':
+        return [1, 2]
+    
+    #Si las posiciones [1,1] y [1,2] ya estan llenados por nuestro player ID y la posición [1,0] esta vacia, entonces, retornar la posición [1,0].
+    elif board[1][1] == player_id and board[1][2] == player_id and board[1][0] == '-':
+        return [1, 0]
+    
+    #Si las posiciones [1,2] y [1,0] ya estan llenados por nuestro player ID y la posición [1,1] esta vacia, entonces, retornar la posición [1,1].
+    elif board[1][2] == player_id and board[1][0] == player_id and board[1][1] == '-':
+        return [1, 1]
+
+    #Si las posiciones [2,0] y [2,1] ya estan llenados por nuestro player ID y la posición [2,2] esta vacia, entonces, retornar la posición [2,2].
+    elif board[2][0] == player_id and board[2][1] == player_id and board[2][2] == '-':
+        return [2, 2]
+    
+    #Si las posiciones [2,1] y [2,2] ya estan llenados por nuestro player ID y la posición [2,0] esta vacia, entonces, retornar la posición [2,0].
+    elif board[2][1] == player_id and board[2][2] == player_id and board[2][0] == '-':
+        return [2, 0]
+    
+    #Si las posiciones [2,0] y [2,2] ya estan llenados por nuestro player ID y la posición [2,1] esta vacia, entonces, retornar la posición [2,1].
+    elif board[2][0] == player_id and board[2][2] == player_id and board[2][1] == '-':
+        return [2, 1]
+    
+    
+    
 def validate_move(board: list, move: list) -> bool:
     """
     Checks if the desired next move hits an empty position.
